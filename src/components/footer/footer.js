c488a0b6ce7d072d3a87dc2c385af2fc2b80ce07
Brandon Hoffmann <hoffm670@gmail.com>
2020-01-11 02:52:28 -0600

    Initial commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Footer() {
+  return (
+    <footer>
+        <div class="contact-buttons">
+            <a href="/">Email</a>
+            <a href="/">LinkedIn</a>
+            <a href="/">Github</a>
+        </div>
+    </footer>
+  );
+}
---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Header() {
+  return (
+    <nav>
+        <div class="navbar">
+            <button>Home</button>
+            <button>About Me</button>
+            <button>Contact</button>
+        </div>
+    </nav>
+  );
+}
